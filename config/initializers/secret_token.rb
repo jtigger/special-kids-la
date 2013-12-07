4ce7ab62be46d258598f59af42088ac23a94a07f
John S. Ryan <jtigger@infosysengr.com>
2013-12-07 14:42:52 -0800

    Inception.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpecialKidsLa::Application.config.secret_key_base = '688255be597b56e4f7a85b933b5f326d4ec5072a0d6710e080ffdef32ac0c9bf7f77fd26426b08d74353d3c8d0948e84c717300a38c4842b739d876a88c6007b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpecialKidsLa::Application.config.session_store :cookie_store, key: '_special-kids-la_session'
